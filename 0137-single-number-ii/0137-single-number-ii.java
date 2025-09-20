96abc4dbb86253135970b90b66bec7c0d74dcc4b
Darshan Hase <hasedarshan123@gmail.com>
2025-09-20 22:42:37 +0530

    Time: 3 ms (68.14%), Space: 45.6 MB (54.74%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,22 +1,46 @@
+// class Solution {
+//     public int singleNumber(int[] nums) {
+        
+        
+
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for(int num : nums){
+//             map.put(num, map.getOrDefault(num, 0)+1);
+
+//         }
+//          for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+
+//             if(entry.getValue() == 1){
+//                 return entry.getKey();
+//             }
+        
+//          }
+//              return -1;
+
+    
+// }}
+          
 class Solution {
     public int singleNumber(int[] nums) {
         
-        
+        int result = 0;
 
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0)+1);
+        for(int i = 0;i < 32; i++){
+            int sum = 0;
+            for(int num : nums){
+                if((num>>i & 1) == 1){
+                    sum++;
+                }
+            }
+            if(sum%3 != 0){
+                result  = result | 1<<i;
+            }
 
         }
-         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+        return result;
 
-            if(entry.getValue() == 1){
-                return entry.getKey();
-            }
-        
-         }
-             return -1;
+    }
+}
 
-    
-}}
+
