81370ddd40688f63a541979ce44fd594ecb6da53
Darshan Hase <hasedarshan123@gmail.com>
2025-09-20 12:24:37 +0530

    Time: 0 ms (100%), Space: 50.7 MB (21.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,37 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int unique = 0;
-        for(int n : nums){
-            unique ^= n;
+    // bitwise approach
+    
+        // int unique = 0;
+        // for(int n : nums){
+        //     unique ^= n;
 
+        // }
+        // return unique;
+    
+    //binart search why ?? if it perfectly paired each element in pair will be at evenn index
+
+
+    int low = 0; int high = nums.length -1;
+
+    while(low < high){
+        int mid = low + (high - low)/2;
+
+        if(mid%2 == 1) mid--;   // is mid is odd ? make even
+
+        if(nums[mid] == nums[mid+1]){
+            low = mid + 2;
         }
-        return unique;
+
+        else{
+            high = mid;
+        }
+
     }
-}
+    return nums[low];
+        }
+
+
+
+    
+    }
