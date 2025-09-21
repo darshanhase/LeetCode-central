4f583edd89f2bdf1e867955f1b485f31136e9dcf
Darshan Hase <hasedarshan123@gmail.com>
2025-09-21 17:57:42 +0530

    Time: 0 ms (100%), Space: 40.5 MB (69.91%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,43 +1,43 @@
-// class Solution {
-//     public int numberOfSteps(int num) {
-
-//         return helper(num , 0);
-    
-//     }
-
-//     public int helper(int num, int steps){
-//         if(num == 0){
-//             return steps;
-//         }
-
-//         if(num % 2 == 0){
-//             return helper(num/2, steps+1);
-//         }
-
-//         else{
-//             return helper(num-1, steps+1);
-//         }
-
-
-//     }
-// }
-
-
-
-
 class Solution {
-    public int numberOfSteps(int n) {
-        int count = 0;
-       while(n>0){
-         if(n%2 == 0){
-            n =n/2;
-            count++;
-         }else{
-         n -=1;
-         count++;
-         }
+    public int numberOfSteps(int num) {
+
+        return helper(num , 0);
+    
+    }
+
+    public int helper(int num, int steps){
+        if(num == 0){
+            return steps;
         }
-        System.out.println(count);
-        return count; 
+
+        if(num % 2 == 0){
+            return helper(num/2, steps+1);
+        }
+
+        else{
+            return helper(num-1, steps+1);
+        }
+
+
     }
 }
+
+
+
+
+// class Solution {
+//     public int numberOfSteps(int n) {
+//         int count = 0;
+//        while(n>0){
+//          if(n%2 == 0){
+//             n =n/2;
+//             count++;
+//          }else{
+//          n -=1;
+//          count++;
+//          }
+//         }
+//         System.out.println(count);
+//         return count; 
+//     }
+// }
