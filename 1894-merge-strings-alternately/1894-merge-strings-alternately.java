0ca5a3d913630363d823696bfc840f585a0ac68c
Darshan Hase <hasedarshan123@gmail.com>
2025-09-24 12:21:48 +0530

    Time: 1 ms (81.19%), Space: 41.9 MB (57.84%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,30 +1,52 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-    String merged = "";
-    int i = 0;
-    int j = 0;
+    // String merged = "";
+    // int i = 0;
+    // int j = 0;
 
-    while(i < word1.length() && j < word2.length()){
+    // while(i < word1.length() && j < word2.length()){
         
-        merged = merged + word1.charAt(i);
-        merged = merged + word2.charAt(j);
-        i++;
-        j++;
+    //     merged = merged + word1.charAt(i);
+    //     merged = merged + word2.charAt(j);
+    //     i++;
+    //     j++;
         
 
+    // }
+    // while(i < word1.length()){
+
+    //     merged = merged + word1.charAt(i);
+    //     i++;
+    // } 
+    // while(j < word2.length()){
+
+    //     merged = merged + word2.charAt(j);
+    //     j++;
+    // }
+
+    // return merged;
+
+
+
+  StringBuilder merged = new StringBuilder();
+        int i = 0, j = 0;
+
+      
+        while (i < word1.length() && j < word2.length()) {
+            merged.append(word1.charAt(i++));
+            merged.append(word2.charAt(j++));
+        }
+
+        
+        while (i < word1.length()) {
+            merged.append(word1.charAt(i++));
+        }
+
+        
+        while (j < word2.length()) {
+            merged.append(word2.charAt(j++));
+        }
+
+        return merged.toString();
     }
-    while(i < word1.length()){
-
-        merged = merged + word1.charAt(i);
-        i++;
-    } 
-    while(j < word2.length()){
-
-        merged = merged + word2.charAt(j);
-        j++;
     }
-
-    return merged;
-
-    }
-}
