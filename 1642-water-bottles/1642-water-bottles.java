944fef98fae7fa095572679c4ac73cc9121c708d
Darshan Hase <hasedarshan123@gmail.com>
2025-10-01 22:44:43 +0530

    Time: 0 ms (100%), Space: 40.2 MB (86.01%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,14 +1,18 @@
 class Solution {
+    // public int numWaterBottles(int numBottles, int numExchange) {
+    //       int totalDrank = numBottles;
+    //     int empty = numBottles;
+
+    //     while (empty >= numExchange) {
+    //         int newBottles = empty / numExchange;
+    //         totalDrank += newBottles;
+    //         empty = empty % numExchange + newBottles; 
+    //     }
+
+    //     return totalDrank;
+    // }
     public int numWaterBottles(int numBottles, int numExchange) {
-          int totalDrank = numBottles;
-        int empty = numBottles;
-
-        while (empty >= numExchange) {
-            int newBottles = empty / numExchange;
-            totalDrank += newBottles;
-            empty = empty % numExchange + newBottles; 
-        }
-
-        return totalDrank;
+       
+        return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 }
