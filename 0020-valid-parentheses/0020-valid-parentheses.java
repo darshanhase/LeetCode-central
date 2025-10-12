abc6a442c902d03e879412f6b646768c44f2882e
Darshan Hase <hasedarshan123@gmail.com>
2025-10-12 23:20:18 +0530

    Time: 3 ms (55.1%), Space: 41.9 MB (70.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,12 @@
 class Solution {
+
+    //we can use char[] stack ---- char array as stack  it reduces java's overhead for stack class but it also will take O(N) 
+
+
     public boolean isValid(String s) {
 
         if (s.length() % 2 != 0) return false;
-        
+
          Stack<Character> stack = new Stack<>();
 
         for (char ch : s.toCharArray()) {
