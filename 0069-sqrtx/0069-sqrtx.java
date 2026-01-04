cab5c4a1064e2eee29b479f1a84923ae05da5ce8
Darshan Hase <hasedarshan123@gmail.com>
2026-01-05 00:19:44 +0530

    Time: 25 ms (5.13%), Space: 42.7 MB (21.35%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,24 +1,33 @@
 class Solution {
-    public int mySqrt(int x) {
-           if (x < 2) return x;  // sqrt(0) = 0, sqrt(1) = 1
 
-        int left = 1, right = x / 2;  // sqrt(x) is always <= x/2 for x >= 2
-        int ans = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            long sq = (long) mid * mid;  // use long to avoid overflow
-
-            if (sq == x) {
-                return mid;  // perfect square
-            } else if (sq < x) {
-                ans = mid;   // store candidate
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+public int mySqrt(int x){
+     if(x==0) return 0;
+        long val=1;
+        while(val*val<=x){
+            val++;
         }
-        return ans;  // largest integer whose square <= x
+        return (int)val-1;
+}
+    // public int mySqrt(int x) {
+    //        if (x < 2) return x;  // sqrt(0) = 0, sqrt(1) = 1
 
-    }
+    //     int left = 1, right = x / 2;  // sqrt(x) is always <= x/2 for x >= 2
+    //     int ans = 0;
+
+    //     while (left <= right) {
+    //         int mid = left + (right - left) / 2;
+    //         long sq = (long) mid * mid;  // use long to avoid overflow
+
+    //         if (sq == x) {
+    //             return mid;  // perfect square
+    //         } else if (sq < x) {
+    //             ans = mid;   // store candidate
+    //             left = mid + 1;
+    //         } else {
+    //             right = mid - 1;
+    //         }
+    //     }
+    //     return ans;  // largest integer whose square <= x
+
+    // }
 }
