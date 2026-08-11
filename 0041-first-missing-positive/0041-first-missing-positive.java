b14e9642ea5f266a04a4121be3656bfcfa053726
Darshan Hase <hasedarshan123@gmail.com>
2026-08-11 23:03:43 +0530

    Time: 2 ms (35.52%), Space: 71 MB (86.12%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,22 +1,48 @@
 class Solution {
+    // public  void swap(int[] nums, int i ,int j ){
+    //     nums[i] = nums[i]^nums[j];
+    //     nums[j] = nums[i]^nums[j];
+    //     nums[i] = nums[i]^nums[j];
+        
+    // }
     public int firstMissingPositive(int[] nums) {
 
-        HashSet<Integer> set=new HashSet<>();
-        int mini = Integer.MAX_VALUE;
-        for(int i:nums){
-            if(i < 0) continue;
-            set.add(i); 
-            mini = Math.min(mini,i);
+        // HashSet<Integer> set=new HashSet<>();
+        // int mini = Integer.MAX_VALUE;
+        // for(int i:nums){
+        //     if(i < 0) continue;
+        //     set.add(i); 
+        //     mini = Math.min(mini,i);
+        // }
+
+        // if( mini > 1){
+        //     return 1;
+        // }
+
+        // while(set.contains(mini)){
+        //     mini++;
+        // }
+
+        // return mini;
+
+
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                i++;
+            }
         }
 
-        if( mini > 1){
-            return 1;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                return j+1;
+            }
         }
-
-        while(set.contains(mini)){
-            mini++;
-        }
-
-        return mini;
+        return nums.length+1;
     }
-}
+}  
